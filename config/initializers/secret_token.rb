f89a60eacb41b0aba3c51b232057ab4ac73f28da
friermor <david@classwave.com>
2012-02-03 09:51:09 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileLocker::Application.config.secret_token = '14513da03cfc75b8e92fae246d20b42fdae3d7751f0bf9655c91cd1fcbad044ebeeab82863c1125fa0aa436589d2b3c1f3001dc0f3b537d6cb2bd6db18f8abf9'
